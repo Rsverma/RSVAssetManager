52a8e2fc003e3e05dfaa2101839ae706fbe71f7f
Ramesh Verma <rsverma333@gmail.com>
2021-03-15 21:42:45 +0530

    Minor Import related changes

--- RAMDesktopUI/ViewModels/ImportManagerViewModel.cs
+++ RAMDesktopUI/ViewModels/ImportManagerViewModel.cs
@@ -1,6 +1,9 @@
-﻿using RAMDesktopUI.Controls;
+﻿using Microsoft.Win32;
+using RAMDesktopUI.Controls;
+using RAMDesktopUI.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,9 +27,36 @@ namespace RAMDesktopUI.ViewModels
             return true;
         }
 
-
         void SelectionChanged(object value)
         {
+        }
+
+        private DataTable _importableRows = new();
+
+        public DataTable ImportableRows
+        {
+            get { return _importableRows; }
+            set
+            {
+                _importableRows = value;
+                NotifyOfPropertyChange(() => ImportableRows);
+            }
+        }
+
+
+        public void ImportFile()
+        {
+            OpenFileDialog openFileDialog = new();
+            openFileDialog.Filter = "Excel or CSV Files|*.csv;*.xls;*.xlsx;*.xlsb";
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                DataSet data = ExcelCSVHelper.GetDataSetFromFile(openFileDialog.FileName);
+                if (data != null && data.Tables.Count > 0 && data.Tables[0].Columns.Count > 0)
+                {
+                    ImportableRows = data.Tables[0];
+                }
+            }
         }
     }
 }
